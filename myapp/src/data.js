e32087557dac5b4fa3e6220bbf3f3dc5c4316f99
Ankit Kumar <115263351+Ankitkumar7571@users.noreply.github.com>
2025-12-28 03:19:32 +0530

    Initial commit - Plan With Love

---
+++ myapp/src/data.js
@@ -0,0 +1,58 @@
+const data = [
+       {
+        id:1,
+        name:"Agra",
+        info:"Agra is primarily famous for the Taj Mahal, a white marble mausoleum and one of the Seven Wonders of the World. It's also known for the Agra Fort, another UNESCO World Heritage Site and the former residence of the Mughal emperors. Additionally, Fatehpur Sikri, another UNESCO World Heritage Site and former Mughal capital, is a significant attraction in the area.",
+        image:"https://upload.wikimedia.org/wikipedia/commons/6/68/Taj_Mahal%2C_Agra%2C_India.jpg",
+        price:"35,758"
+      },
+      {  id: 2,
+        name:"Jaipur",
+        info:"Jaipur, the capital of Rajasthan, is known as the Pink City due to its distinctively colored buildings. It's famous for its historic palaces, forts, and vibrant bazaars. Key attractions include the Hawa Mahal (Palace of Winds), Amer Fort, City Palace, and Jantar Mantar.",
+        image:"https://static.toiimg.com/thumb/115224983/Jaipur.jpg?width=636&height=358&resize=4",
+        price:"25,758",
+      },  
+      {
+        id: 3,
+        name:"Delhi",
+        info:"Delhi, the capital of India, is a bustling metropolis that blends history and modernity. It's known for its rich cultural heritage, historical landmarks like the Red Fort, India Gate, and Qutub Minar, as well as its vibrant markets and diverse cuisine.",
+        image:"https://s7ap1.scene7.com/is/image/incredibleindia/india-gate-delhi-1-attr-hero?qlt=82&ts=1727351922349",
+        price:"15,758",
+      },
+      {
+        id: 4,
+        name:"Varanasi",
+        info:"Varanasi, one of the oldest continuously inhabited cities in the world, is a major cultural and religious hub in India. It's famous for its ghats along the Ganges River, where pilgrims perform rituals and ceremonies. The city is also known for its vibrant arts scene and spiritual significance.",
+        image:"https://s7ap1.scene7.com/is/image/incredibleindia/manikarnika-ghat-city-hero?qlt=82&ts=1727959374496",
+        price:"45,758",
+      },
+     {
+        id: 5,
+        name:"Kerala",
+        info:"Kerala, known as 'God's Own Country,' is famous for its lush landscapes, backwaters, and unique culture. It's renowned for its Ayurvedic treatments, houseboat cruises in the backwaters, and beautiful beaches. The state is also known for its rich traditions in art, dance, and cuisine.",
+        image:"https://lp-cms-production.imgix.net/2024-08/shutterstock2044878389.jpg?auto=format,compress&q=72&w=1440&h=810&fit=crop",
+        price:"5,758",
+      },
+      {
+        id: 6,
+        name:"Goa",
+        info:"Goa is famous for its stunning beaches, vibrant nightlife, and Portuguese-influenced architecture. It's a popular tourist destination known for its relaxed atmosphere, water sports, and beach parties. Goa also has a rich cultural heritage with numerous churches and temples.",
+        image:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/33/fc/f0/goa.jpg?w=2400&h=-1&s=1",
+        price:"25,758",
+      },
+      {
+        id: 7,
+        name:"Mumbai",
+        info:"Mumbai, the financial capital of India, is known for its bustling city life, Bollywood film industry, and diverse culture. It's famous for landmarks like the Gateway of India, Marine Drive, and Chhatrapati Shivaji Maharaj Terminus. Mumbai is also known for its vibrant street food scene and cultural festivals.",
+        image:"https://images.contentstack.io/v3/assets/blt00454ccee8f8fe6b/blt3f1dd78cb088d203/61d6b8af0d4d1813c7f45eb5/US_Mumbai_IN_Header.jpg?width=1440&quality=70&auto=webp",
+        price:"35,758",
+      },
+       {
+        id: 8,
+        name:"Rajasthan",
+        info:"Rajasthan is famous for its royal heritage, palaces, and desert landscapes. It's known for cities like Jaipur, Udaipur, Jodhpur, and Jaisalmer, each with its unique charm. The state is also renowned for its colorful festivals, traditional crafts, and rich history.",
+        image:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/33/fc/f9/rajasthan.jpg?w=2400&h=-1&s=1",
+        price:"45,758",
+    }
+]
+export default data;
